8ff875cbeb32a63bfbe98193aece628f48c4b0e0
Reina <reinasosa2024@gmail.com>
2026-06-03 16:33:07 -0600

    users

--- src/rentals/rental.entity.ts
+++ src/rentals/rental.entity.ts
@@ -1,1 +1,14 @@
-export class RentalEntity {}
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, ManyToOne} from "typeorm";
+import { MachineEntity } from "src/machines/machine.entity";
+import { UserEntity } from "src/users/user.entity";
+
+export class RentalEntity {
+       @PrimaryGeneratedColumn()
+       id: number;
+
+       @ManyToOne(() => UserEntity, (user) => user.rentals)
+       user: UserEntity;
+
+       @ManyToOne(() => MachineEntity, (machine) => machine.rentals)
+       machine: MachineEntity;
+}
