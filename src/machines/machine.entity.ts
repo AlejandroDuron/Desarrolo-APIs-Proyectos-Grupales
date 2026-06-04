bc6171d3058568756f62d753ba75b9977c5f31e2
Jhonnatan Ezequiel Peñate Serrano <20245184@esen.edu.sv>
2026-06-03 19:17:18 -0600

    Configuracion de machine entity y tood el flujo

--- src/machines/machine.entity.ts
+++ src/machines/machine.entity.ts
@@ -1,11 +1,33 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany, ManyToOne} from "typeorm";
-import { UserEntity } from "src/users/user.entity";
-import { RentalEntity } from "src/rentals/rental.entity";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, ManyToOne } from 'typeorm';
+import { UserEntity } from 'src/users/user.entity';
+import { RentalEntity } from 'src/rentals/rental.entity';
+
+export enum MachineStatus {
+       DISPONIBLE = 'Disponible',
+       ALQUILADA = 'Alquilada',
+       EN_TALLER = 'En Taller',
+}
 
 @Entity()
 export class MachineEntity {
        @PrimaryGeneratedColumn()
        id: number;
+
+       @Column()
+       name: string;
+
+       @Column()
+       description: string;
+
+       @Column('decimal', { precision: 10, scale: 2, default: 0 })
+       price: number;
+
+       @Column({
+              type: 'enum',
+              enum: MachineStatus,
+              default: MachineStatus.EN_TALLER,
+       })
+       status: MachineStatus;
 
        @ManyToOne(() => UserEntity, (user)=> user.machines, {eager: true})
        user: UserEntity;
--- src/machines/machines.controller.ts
+++ src/machines/machines.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Param } from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { MachinesService } from './machines.service';
 
+@ApiTags('Machines')
 @Controller('machines')
-export class MachinesController {}
+export class MachinesController {
+	constructor(private readonly machinesService: MachinesService) {}
+
+	@Delete(':id')
+	@ApiOperation({ summary: 'Eliminar una maquina por ID' })
+	@ApiParam({ name: 'id', description: 'ID de la maquina' })
+	@ApiResponse({ status: 200, description: 'Maquina eliminada correctamente.' })
+	@ApiResponse({ status: 400, description: 'La maquina tiene solicitudes activas.' })
+	@ApiResponse({ status: 404, description: 'Maquina no encontrada.' })
+	delete(@Param('id') id: string): Promise<void> {
+		return this.machinesService.deleteMachine(+id);
+	}
+}
